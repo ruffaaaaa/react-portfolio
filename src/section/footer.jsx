4080a5d44ba7af9a1bc289abd323467a1bc2ee4d
ruffaaaaa <99468986+ruffaaaaa@users.noreply.github.com>
2024-12-30 01:43:55 +0800

    Update footer.jsx

--- src/section/footer.jsx
+++ src/section/footer.jsx
@@ -4,7 +4,7 @@ function Footer() {
     return (
         <div className="relative z-0 h-full rounded-t-3xl bg-gradient-to-r from-[#53368A] to-[#CA60B8] pt-[14rem] pb-8 -mt-[12rem] flex items-end">
             <div className="text-white text-center w-full">
-                <p className="text-xs lg:text-sm">
+                <p className="text-[12px] lg:text-sm">
                     © Copyright 2024. Developed and Designed by{' '}
                     <span className="font-bold">Ruffa Mae Arañez</span>
                 </p>
